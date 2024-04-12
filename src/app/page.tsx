b2de2b4eea1ef27e2713dacae2af4580ede6515b
Iker Garcia <A01782767@tec.mx>
2024-04-11 19:24:11 -0600

    Commented amplify for visualization.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import awsExports from "@/aws-exports";
+// import awsExports from "@/aws-exports";
 import { TextGenerateEffect } from "@/ui/text-generate-effect";
 import { WavyBackground } from "@/ui/wavy-background";
 
@@ -7,7 +7,7 @@ import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import { signOut } from "aws-amplify/auth";
 
-Amplify.configure(awsExports);
+// Amplify.configure(awsExports);
 
 export default function Home() {
   const handleSignOut = async () => {
