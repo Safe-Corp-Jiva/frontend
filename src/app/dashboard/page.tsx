e49184269e01a612cbf6d16c5e33811ffd6ac6e0
Cristina <A01025667@tec.mx>
2024-04-14 14:33:12 -0600

    Se agregó el componente navbar y el dashboard (sin funcionalidad)

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import NavBar from "../components/navBar";
+
+export default function Home() {
+    return (
+        <div className = "flex bg-gray-200">
+            <NavBar/>
+        </div>
+    );
+};
