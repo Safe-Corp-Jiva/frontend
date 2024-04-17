a9e87ceffa8c7a253ba896ced3aa0745a9d43b87
Cristina <A01025667@tec.mx>
2024-04-16 19:04:29 -0600

    Arreglos de logout view

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -18,16 +18,22 @@ export default function Home() {
                     </div>
                     <p className = "text-center text-2xl text-black font-bold mt-3">John Doe</p>
                     <p className = "text-center text-sm text-black mt-1">Supervisor - SP0001</p>
-                    <div className = "grid grid-cols-2 gap-4 py-10 w-1/2 mx-auto">
-                            <p className = "text-black font-bold text-sm ml-20 mr-30">Email</p>
+                    <div className = "gap-6 py-10 w-1/3 mx-auto flex flex-col">
+                        <div className="flex justify-between">
+                            <p className = "text-black font-bold text-sm">Email</p>
                             <p className = "text-gray-500 text-sm">john.doe@jivaa.com</p>
-                            <p className = "text-black font-bold text-sm ml-20 mr-30">Date of birth</p>
+                        </div>
+                        <div className="flex justify-between">
+                            <p className = "text-black font-bold text-sm">Date of birth</p>
                             <p className = "text-gray-500 text-sm">01/08/2000</p>
-                            <p className = "text-black font-bold text-sm ml-20 mr-30">Department</p>
+                        </div>
+                        <div className="flex justify-between">
+                            <p className = "text-black font-bold text-sm">Department</p>
                             <p className = "text-gray-500 text-sm">Sales</p>
+                        </div>
                     </div> 
-                    <div className = "flex items-center justify-center mt-8">  
-                        <button onClick={handleSignOut} className = "bg-gradient-to-r from-blue-900 to-teal-400 px-3 py-2 h-250 w-60 rounded-md text-white "> Sign Out </button>
+                    <div className = "flex items-center justify-center mt-6">  
+                        <button onClick={handleSignOut} className = "bg-gradient-to-r from-blue-900 to-teal-400 px-3 py-2 h-250 w-48 rounded-lg text-white "> Sign Out </button>
                     </div>
                 </div>
             </div>
