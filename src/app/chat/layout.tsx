d560b1e2ef2b72cbd9b9eea6194fd4894e506579
omaration21 <A01374690@tec.mx>
2024-04-25 18:03:04 -0600

    'Cambio de diseño a Chat View' #2

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -11,15 +11,15 @@ export default function Layout({ user, admin }: { user: React.ReactNode; admin: 
   const role = CheckUserRole();
 
   return (
-    <div className="h-screen w-screen flex bg-gray-100">
+    <div className="h-screen w-screen flex bg-white justify-center items-center"> {/* Fondo blanco y centrado */}
       <NavBar />
-      <div className="flex flex-1">
-        <div className="w-1/4 bg-white">
+      <div className="flex w-11/12 bg-gray-100 rounded-2xl shadow-xl overflow-hidden" style={{ height: 'calc(100% - 2rem)' }}>
+      <div className="border-r border-gray-300 w-80 bg-white overflow-y-auto">
           <Users />
         </div>
-        <div className="flex-1 flex flex-col">
-          {role === 'admin' ? admin : user}
+        <div className="border-l border-gray-300 flex-1 flex flex-col bg-white rounded-r-2xl">
           <TextBox />
+          {role === 'admin' ? admin : user}
         </div>
       </div>
     </div>
--- src/components/chat/users/index.tsx
+++ src/components/chat/users/index.tsx
@@ -10,19 +10,24 @@ const users = [
     { name: 'Alina Rosas', image: '/icons/User.svg'},
     { name: 'Sebas Jones', image: '/icons/User.svg'},
     { name: 'John Pier', image: '/icons/User.svg'},
+    { name: 'Don Watts', image: '/icons/User.svg'},
+    { name: 'John Such', image: '/icons/User.svg'},
+    { name: 'Lord Jones', image: '/icons/User.svg'},
+    { name: 'Alexandro Pier', image: '/icons/User.svg'},
+    { name: 'Iker Democracy', image: '/icons/User.svg'},
   ];
 
-const Users = () => {
-  return (
-    <div className="overflow-auto">
-      {users.map((user, index) => (
-        <div key={index} className="flex items-center p-4 hover:bg-gray-200 cursor-pointer border-b border-gray-300">
-          <Image src={user.image} alt={user.name} width={32} height={32} className="rounded-full" />
-          <p className="ml-3">{user.name}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default Users;
+  const Users = () => {
+    return (
+      <div className="overflow-auto">
+        {users.map((user, index) => (
+          <div key={index} className="flex items-center p-4 hover:bg-gray-200 cursor-pointer">
+            <Image src={user.image} alt={user.name} width={28} height={28} className="rounded-full" />
+            <p className="ml-3">{user.name}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
+  
+  export default Users;
