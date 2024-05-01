b4853789235d57d7ff89338d2172fe80c67abc5a
ronuma <ronum.00@gmail.com>
2024-04-30 20:10:37 -0600

    Safetest added with a base simple test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,12 @@ import { Authenticator } from '@aws-amplify/ui-react'
 import { Amplify } from 'aws-amplify'
 import { signOut } from 'aws-amplify/auth'
 
+import { MainPageFileOverride } from './Overrides'
+
 // Amplify.configure(awsExports);
 
 export default function Home() {
+  const mainPage = MainPageFileOverride.useValue()
   const handleSignOut = async () => {
     try {
       await signOut()
