1cb6ee1380d2cb30a81aaeb6d5af2fb332017c8b
Iker Garcia <A01782767@tec.mx>
2024-05-16 14:39:23 -0600

    Added Icons

--- src/components/mockdndagents/task/page.tsx
+++ src/components/mockdndagents/task/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Draggable, DraggableProvided, DraggableStateSnapshot } from 'react-beautiful-dnd';
 import { Task as TaskType } from '../types/index';
+import { CircleUser } from 'lucide-react';
 
 interface TaskProps {
   task: TaskType;
@@ -15,8 +16,9 @@ const Task: React.FC<TaskProps> = ({ task, index }) => {
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
-          className={`border border-gray-300 rounded p-2 mb-2 bg-white ${snapshot.isDragging ? 'bg-green-200' : ''}`}
+          className={`border border-gray-300 rounded p-2 mb-2 bg-white flex gap-2 select-none ${snapshot.isDragging ? 'bg-green-200' : ''}`}
         >
+          <CircleUser size={24} color="#434D75" strokeWidth={1} />
           {task.content}
         </div>
       )}
