c23b62b1cda3ac850ac9396fc749418b9b0f4113
Fernanda Cantú <fercantu56@gmail.com>
2024-05-16 20:08:56 -0600

    usar image envez de img

--- src/app/(main)/@agent/dashboard/page.tsx
+++ src/app/(main)/@agent/dashboard/page.tsx
@@ -7,19 +7,19 @@ function page() {
       <div className="bg-white flex flex-col justify-center rounded-xl w-1/3 h-5/6 mx-4">
         <div className="p-3 flex flex-col justify-evenly items-center flex-1">
           <div className="flex flex-col items-center justify-center">
-            <Image src="icons/User_d.svg" alt="user" width={110} height={110} />
+            <Image src="/icons/User_d.svg" alt="user" width={110} height={110} />
             <h1 className="mt-4 font-bold text-xl">5533486343</h1>
             <h4 className="font-bold text-sm">5:37</h4>
           </div>
           <div className="flex flex-row p-3 space-x-5">
             <button className="bg-red-500 rounded-full w-10 h-10 flex justify-center items-center">
-              <img src="icons/telephone.svg" alt="telephone" width={20} height={20} />
+              <Image src="/icons/telephone.svg" alt="telephone" width={20} height={20} />
             </button>
             <button className="bg-blue-400 rounded-full w-10 h-10 flex justify-center items-center">
-              <img src="icons/microphone.svg" alt="microphone" width={20} height={20} />
+              <Image src="/icons/microphone.svg" alt="microphone" width={20} height={20} />
             </button>
             <button className="bg-yellow-400 rounded-full w-10 h-10 flex justify-center items-center">
-              <img src="icons/exclamation-triangle.svg" alt="triangle" width={20} height={20} />
+              <Image src="/icons/exclamation-triangle.svg" alt="triangle" width={20} height={20} />
             </button>
           </div>
         </div>
