f377489de1276a1616ad11c78f374eb0bcfaab9a
Sebastian Moncada <sebas.200221@gmail.com>
2024-05-16 21:15:42 -0600

    Better design on the maximized card

--- src/components/dash/trendingCard/index.tsx
+++ src/components/dash/trendingCard/index.tsx
@@ -1,6 +1,6 @@
 'use client'
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 interface Props {
   maximize: () => void
@@ -13,36 +13,72 @@ export default function TrendingCard({ maximize, minimize, isMaximized }: Props)
 
   const trendingTopics = [
     {
-      topic: "Lost Flight",
-      quantity: "1282858",
+      topic: 'Lost Flight',
+      quantity: '1282858',
     },
     {
-      topic: "Booking",
-      quantity: "346768",
+      topic: 'Booking',
+      quantity: '346768',
     },
     {
-      topic: "Reservations",
-      quantity: "97451",
+      topic: 'Reservations',
+      quantity: '97451',
     },
     {
-      topic: "Compliance",
-      quantity: "8723",
+      topic: 'Compliance',
+      quantity: '8723',
     },
     {
-      topic: "Trip Organization",
-      quantity: "549",
+      topic: 'Trip Organization',
+      quantity: '549',
     },
     {
-      topic: "Change Flight",
-      quantity: "12",
+      topic: 'Change Flight',
+      quantity: '12',
+    },
+    {
+      topic: 'Change Flight',
+      quantity: '12',
+    },
+    {
+      topic: 'Change Flight',
+      quantity: '12',
+    },
+    {
+      topic: 'Change Flight',
+      quantity: '12',
+    },
+    {
+      topic: 'Change Flight',
+      quantity: '12',
+    },
+    {
+      topic: 'Change Flight',
+      quantity: '12',
+    },
+    {
+      topic: 'Change Flight',
+      quantity: '12',
+    },
+    {
+      topic: 'Change Flight',
+      quantity: '12',
+    },
+    {
+      topic: 'Change Flight',
+      quantity: '12',
+    },
+    {
+      topic: 'Change Flight',
+      quantity: '12',
     },
   ]
-  trendingTopics.sort((a, b) => parseInt(b.quantity) - parseInt(a.quantity));
+  trendingTopics.sort((a, b) => parseInt(b.quantity) - parseInt(a.quantity))
 
   return (
-    <div className = "w-full h-full bg-white rounded-xl flex flex-col p-4">
-      <div className = "flex justify-between mb-4">
-        <h1 className="text-gray-400 text-xl md:text-2xl">Most Requested Topics</h1>
+    <div className="w-full h-full bg-white rounded-xl flex flex-col p-4">
+      <div className="flex justify-between mb-4">
+        <h1 className={`text-gray-400 ${isMaximized ? 'text-2xl' : 'text-xl'}`}>Most Requested Topics</h1>
         {isMaximized ? (
           <button onClick={minimize}>
             <Image src="/icons/expand.svg" alt="home" width={16} height={16} />
@@ -53,13 +89,18 @@ export default function TrendingCard({ maximize, minimize, isMaximized }: Props)
           </button>
         )}
       </div>
-      <div className = "h-full overflow-y-auto">
+      <div className="h-full overflow-y-auto">
         {trendingTopics.map((topic, index) => (
-          <div className = "flex flex-row justify-between items-center h-1/5 border-b-2 border-gray-200 mx-4 px-4 gap-4" key={index}>
-            <h1 className = "flex-none text-gray-400">{index + 1}.-</h1>
-            <h1 className = "flex-1">{topic.topic}</h1>
-            <h1 className = "text-right text-gray-400">{topic.quantity}</h1>
-            <Image src = "/icons/barChart.svg" alt="home" width={24} height={24} />
+          <div
+            className={`flex flex-row justify-between items-center border-b-2 border-gray-200 mx-4 px-4 gap-4 ${
+              isMaximized ? 'h-[10%]' : 'h-1/5'
+            }`}
+            key={index}
+          >
+            <h1 className="flex-none text-gray-400">{index + 1}.-</h1>
+            <h1 className="flex-1">{topic.topic}</h1>
+            <h1 className="text-right text-gray-400">{topic.quantity}</h1>
+            <Image src="/icons/barChart.svg" alt="home" width={24} height={24} />
           </div>
         ))}
       </div>
