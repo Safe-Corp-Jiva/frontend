865a2efb0bd1fcfdecbbc23560086e92a38113ca
Rodrigo Nuñez <ronum.00@gmail.com>
2024-06-03 23:46:08 -0600

    Added list documents for the page

--- src/app/(main)/@supervisor/documents/page.tsx
+++ src/app/(main)/@supervisor/documents/page.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
 import { BottomNav } from '@/components/docs'
+import { listDocuments } from '@/api/documents'
 
-const documents = ['Document 1', 'Document 2', 'Document 3', 'Document 4', 'Document 5', 'Document 6']
+// const documents = ['Document 1', 'Document 2', 'Document 3', 'Document 4', 'Document 5', 'Document 6']
 
-export default function Documents() {
+export default async function Documents() {
+  const documents = await listDocuments()
   return (
     <div className="w-full h-full flex flex-col items-center justify-evenly py-5 px-36 gap-5">
       <div className="w-full h-full bg-white rounded-xl flex justify-start divide-x-2 divide-black/20">
         <div className="h-full w-[13.3333%] rounded-l-xl">
           <div className="bg-SCJ-dark-primary rounded-tl-xl py-3 text-center text-white">Documents</div>
           <div className="flex flex-col w-full items-center divide-y-2">
-            {documents.map((document, index) => (
+            {/* {documents.map((document, index) => (
               <div key={index} className="w-full text-center py-2">
                 {document}
               </div>
-            ))}
+            ))} */}
           </div>
         </div>
         <div className="w-full h-full flex justify-center items-center">Document View</div>
