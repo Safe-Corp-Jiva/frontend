23058b16b9e61591c55b6b590ae36fa9ebb12f0f
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2024-06-06 08:04:02 -0600

    Drag and drop is synced (with delay) with backend

--- src/components/mockdndagents/data/initalData.js
+++ src/components/mockdndagents/data/initalData.js
@@ -1,31 +1,24 @@
 const initialData = {
-  tasks: {
-    'agent-1': { id: 'agent-1', content: 'Agent John Doe' },
-    'agent-2': { id: 'agent-2', content: 'Agent Jane Doe' },
-    'agent-3': { id: 'agent-3', content: 'Agent John Wick' },
-    'agent-4': { id: 'agent-4', content: 'Agent Saitama' },
-    'agent-5': { id: 'agent-5', content: 'Agent Rodrigo Nuñez' },
-    'agent-6': { id: 'agent-6', content: 'Agent Fer Cantu' },
-  },
+  tasks: { },
   columns: {
-    'column-1': {
-      id: 'column-1',
+    'Bookings': {
+      id: 'Bookings',
       title: 'Booking',
-      taskIds: ['agent-1', 'agent-4'],
+      taskIds: [],
     },
-    'column-2': {
-      id: 'column-2',
-      title: 'Cancelations',
-      taskIds: ['agent-2', 'agent-5'],
+    'Cancellations': {
+      id: 'Cancellations',
+      title: 'Cancellations',
+      taskIds: [],
     },
-    'column-3': {
-      id: 'column-3',
+    'Information': {
+      id: 'Information',
       title: 'Information',
-      taskIds: ['agent-3', 'agent-6'],
+      taskIds: [],
     },
   },
-  // Facilitate reordering of the columns
-  columnOrder: ['column-1', 'column-2', 'column-3'],
+  
+  columnOrder: ['Bookings', 'Cancellations', 'Information'],
 }
 
 export default initialData
