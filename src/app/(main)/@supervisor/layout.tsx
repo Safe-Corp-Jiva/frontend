dc42d5446c39bdcb226c153b773c7de39e1adebf
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2024-06-06 01:53:39 -0600

    Add copilot to layout for supervisors! 📎

--- src/app/(main)/@supervisor/layout.tsx
+++ src/app/(main)/@supervisor/layout.tsx
@@ -2,12 +2,16 @@
 import { NavBar } from '@/components/nav'
 import { usePathname } from 'next/navigation'
 import React, { Suspense } from 'react'
+import { ChatBot } from '@/components/dash'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-full h-full flex">
       <NavBar />
-      <Suspense>{children}</Suspense>
+      <Suspense>
+        {children}
+      </Suspense>
+      <ChatBot />
     </div>
   )
 }
