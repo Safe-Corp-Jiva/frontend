3ccee3bc9d29b788f9f3d3254963dfd2a3b36a16
Iker Garcia <A01782767@tec.mx>
2024-06-10 20:14:19 -0600

    Issue Dashboard enhancements #88 Finished Commit

--- src/components/dash/metricsCard/index.tsx
+++ src/components/dash/metricsCard/index.tsx
@@ -5,6 +5,7 @@ import Line from 'rc-progress/es/Line'
 import { getMetrics } from '@/api/connect'
 import { useEffect, useState } from 'react'
 import { useOngoingCalls } from '../onGoingCallsCard/hooks'
+import { Tooltip, TooltipTrigger, TooltipContent } from './tooltip'
 
 interface Props {
   maximize: () => void
@@ -26,13 +27,31 @@ const ProgressBar = ({ percent, title, _strokeColor, _trailColor, isMaximized }:
   const textSizeClass = isMaximized ? 'text-2xl' : 'text-xl'
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full">
-      <div className={`mb-2 text-center font-semibold${titleClass}`}>{title}</div>
-      <div className={`relative ${sizeClass}`}>
-        <Circle percent={percent} strokeWidth={10} trailWidth={8} strokeColor={_strokeColor} trailColor={_trailColor} />
-        <div className={`absolute inset-0 flex items-center justify-center ${textSizeClass}`}>{percent}%</div>
+    <Tooltip>
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <TooltipTrigger asChild={true}>
+          <div className={`mb-2 text-center font-semibold${titleClass}`}>{title}</div>
+        </TooltipTrigger>
+        <div className={`relative ${sizeClass}`}>
+          <Circle
+            percent={percent}
+            strokeWidth={10}
+            trailWidth={8}
+            strokeColor={_strokeColor}
+            trailColor={_trailColor}
+          />
+          <div className={`absolute inset-0 flex items-center justify-center ${textSizeClass}`}>{percent}%</div>
+        </div>
       </div>
-    </div>
+      <TooltipContent>
+        {/* if Title Agent Ocupancy, then Percentage of time that agents were active on contacts. If title Abandoment Rate: The percent of queued contacts that were disconnected without being connected to an agent. Contacts queued for callback are not counted as abandoned.*/}
+        {title === 'Agent occupancy' ? (
+          <p>The percentage of time that agents were active on contacts.</p>
+        ) : (
+          <p>The percent of queued contacts that were disconnected without being connected to an agent.</p>
+        )}
+      </TooltipContent>
+    </Tooltip>
   )
 }
 
@@ -44,30 +63,30 @@ interface ChangeLineProps {
   isMaximized: boolean
 }
 
-const ChangeLine = ({ percent, title, _strokeColor, _trailColor, isMaximized }: ChangeLineProps) => {
-  const titleClass = isMaximized ? 'text-lg' : 'text-sm'
-  const percentClass = isMaximized ? 'text-lg' : 'text-sm'
-  return (
-    <div className="flex flex-col items-start justify-start w-full h-full">
-      <div className={`mb-2 ${titleClass}`}>{title}</div>
-      <div className="relative w-full">
-        <Line
-          percent={Number(percent)}
-          strokeWidth={1.5}
-          trailWidth={1.5}
-          strokeColor={_strokeColor}
-          trailColor={_trailColor}
-        />
-        <div
-          className={`absolute right-0 -ml-4 flex items-center justify-center ${percentClass}`}
-          style={{ top: '-27px' }}
-        >
-          {percent}
-        </div>
-      </div>
-    </div>
-  )
-}
+// const ChangeLine = ({ percent, title, _strokeColor, _trailColor, isMaximized }: ChangeLineProps) => {
+//   const titleClass = isMaximized ? 'text-lg' : 'text-sm'
+//   const percentClass = isMaximized ? 'text-lg' : 'text-sm'
+//   return (
+//     <div className="flex flex-col items-start justify-start w-full h-full">
+//       <div className={`mb-2 ${titleClass}`}>{title}</div>
+//       <div className="relative w-full">
+//         <Line
+//           percent={Number(percent)}
+//           strokeWidth={1.5}
+//           trailWidth={1.5}
+//           strokeColor={_strokeColor}
+//           trailColor={_trailColor}
+//         />
+//         <div
+//           className={`absolute right-0 -ml-4 flex items-center justify-center ${percentClass}`}
+//           style={{ top: '-27px' }}
+//         >
+//           {percent}
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
 export default function MetricsCard({ maximize, minimize, isMaximized }: Props) {
   const [metrics, setMetrics] = useState([] as any[])
@@ -107,21 +126,25 @@ export default function MetricsCard({ maximize, minimize, isMaximized }: Props) 
     {
       value: ongoingCalls,
       name: 'Ongoing Calls',
+      desc: 'The number of calls currently in progress',
     },
     {
       value: avgContactDuration,
       name: 'Average Contact Duration',
       unit: 's',
+      desc: 'The average time that agents spend on a contact',
     },
     {
       value: avgQueueAnswerTime,
       name: 'Average Queue Answer Time',
       unit: 's',
+      desc: 'The average time that contacts spend in the queue before being answered',
     },
     {
       value: avgNonTalkTime,
       name: 'Average Non-Talk Time',
       unit: 's',
+      desc: 'The average time that agents spend on a contact without talking',
     },
   ]
 
@@ -162,13 +185,20 @@ export default function MetricsCard({ maximize, minimize, isMaximized }: Props) 
         </div>
         <div className={`${isMaximized ? 'mt-10' : 'mt-5'} grid grid-cols-2 gap-4`}>
           {textMetrics.map((metric, index) => (
-            <p key={index} className="text-gray-500">
-              {metric.name}:{' '}
-              <span className="font-bold text-black text-lg">
-                {metric.value || '...'}
-                {metric.unit}
-              </span>
-            </p>
+            <Tooltip>
+              <TooltipTrigger asChild={true}>
+              <p key={index} className="text-gray-500">
+                {metric.name}:{' '}
+                <span className="font-bold text-black text-lg">
+                  {metric.value || '...'}
+                  {metric.unit}
+                </span>
+              </p>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{metric.desc}</p>
+              </TooltipContent>
+            </Tooltip>
           ))}
         </div>
       </div>
