aec7024d3d64ac933750f8515daf1a33106a79d3
Sebastian Moncada <sebas.200221@gmail.com>
2024-06-12 10:16:11 -0600

    Styling little changes

--- src/app/(main)/@agent/CCPGeneral.tsx
+++ src/app/(main)/@agent/CCPGeneral.tsx
@@ -1,11 +1,11 @@
 'use client'
-import { useCCP } from '@/providers/CCPProvider'
-import Image from 'next/image'
-import Wait from './wait/page'
 import ChatBot from '@/components/chat/chatBot'
 import RealTimeTranscript from '@/components/transcpt/realtime'
-import { HelpButton } from './dashboard/HelpButton'
+import { useCCP } from '@/providers/CCPProvider'
+import Image from 'next/image'
 import { usePathname } from 'next/navigation'
+import { HelpButton } from './dashboard/HelpButton'
+import Wait from './wait/page'
 
 const ConnectCCP = () => {
   const {
@@ -23,7 +23,7 @@ const ConnectCCP = () => {
     handleMute,
     handleUnmute,
     setError,
-    setAskedForHelp
+    setAskedForHelp,
   } = useCCP()
 
   const formatTimestampDifference = (start: number, end: number) => {
@@ -38,7 +38,7 @@ const ConnectCCP = () => {
 
   return incomingCall && !callAccepted ? (
     <div
-      className={`absolute top-4 right-1/2 translate-x-1/2 flex flex-row gap-4 items-center justify-center bg-white rounded-xl p-2 shadow-md ${
+      className={`absolute top-4 right-1/2 translate-x-1/2 flex flex-row gap-4 items-center justify-center bg-white rounded-full p-2 px-4 shadow-lg ${
         pathname == '/dashboard' && 'hidden'
       }`}
     >
@@ -54,57 +54,58 @@ const ConnectCCP = () => {
     callAccepted && (
       <div className="sticky right-0 top-0 h-full flex items-center justify-center bg-SCJ-gray">
         <div
-          className={`flex flex-col items-center justify-center bg-white rounded-xl mr-4 p-2 gap-4 shadow-md ${
+          className={`flex flex-col items-center justify-center bg-white rounded-full mr-4 p-2 py-4 gap-4 shadow-lg ${
             pathname == '/dashboard' && 'hidden'
           }`}
         >
-          {/* <p>Call in progress</p> */}
-          <p>{startTimestamp && currentTimestamp && formatTimestampDifference(startTimestamp, currentTimestamp)}</p>
+          <p className="text-sm">
+            {startTimestamp && currentTimestamp && formatTimestampDifference(startTimestamp, currentTimestamp)}
+          </p>
           <div>
-            <button 
-            className="bg-red-500 hover:bg-red-400 rounded-full w-10 h-10 flex justify-center items-center"
-            onClick={handleEndCall}
+            <button
+              className="bg-red-500 hover:bg-red-400 rounded-full w-10 h-10 flex justify-center items-center"
+              onClick={handleEndCall}
             >
-                <Image src="/icons/telephone.svg" alt="telephone" width={20} height={20} />
+              <Image src="/icons/telephone.svg" alt="telephone" width={20} height={20} />
             </button>
           </div>
-            {!isMuted && (
-                <button
-                    className="bg-blue-400 hover:bg-blue-300 rounded-full w-10 h-10 flex justify-center items-center mic-icon"
-                    onClick={handleMute}
-                >
-                    <Image src="/icons/microphone.svg" alt="microphone" width={20} height={20} />
-                </button>
-                )}
-            {isMuted && (
-                <button
-                    className="bg-blue-400 hover:bg-blue-300 rounded-full w-10 h-10 flex justify-center items-center mic-icon muted"
-                    onClick={handleUnmute}
-                >
-                    <Image src="/icons/micMute.svg" alt="microphone" width={20} height={20} />
-                </button>
-                )}
-            {!askedForHelp && (
-                  <HelpButton
-                    callId={currentConnection?.contactId ?? null}
-                    callback={() => {
-                      setError(null)
-                      setAskedForHelp(true)
+          {!isMuted && (
+            <button
+              className="bg-blue-400 hover:bg-blue-300 rounded-full w-10 h-10 flex justify-center items-center mic-icon"
+              onClick={handleMute}
+            >
+              <Image src="/icons/microphone.svg" alt="microphone" width={20} height={20} />
+            </button>
+          )}
+          {isMuted && (
+            <button
+              className="bg-blue-400 hover:bg-blue-300 rounded-full w-10 h-10 flex justify-center items-center mic-icon muted"
+              onClick={handleUnmute}
+            >
+              <Image src="/icons/micMute.svg" alt="microphone" width={20} height={20} />
+            </button>
+          )}
+          {!askedForHelp && (
+            <HelpButton
+              callId={currentConnection?.contactId ?? null}
+              callback={() => {
+                setError(null)
+                setAskedForHelp(true)
 
-                      setTimeout(() => {
-                        setAskedForHelp(false)
-                      }, 5000)
-                    }}
-                    fallback={() => {
-                      setError('Please try again later')
+                setTimeout(() => {
+                  setAskedForHelp(false)
+                }, 5000)
+              }}
+              fallback={() => {
+                setError('Please try again later')
 
-                      setTimeout(() => {
-                        setError(null)
-                      }, 5000)
-                    }}
-                    disabled={askedForHelp}
-                  />
-                )}
+                setTimeout(() => {
+                  setError(null)
+                }, 5000)
+              }}
+              disabled={askedForHelp}
+            />
+          )}
           {error && <p>Error: {error}</p>}
         </div>
       </div>
